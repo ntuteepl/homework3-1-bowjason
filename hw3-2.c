ac760b3ba963301581cdefa159c93e5931cf888d
bowjason <146065148+bowjason@users.noreply.github.com>
2023-10-19 00:21:52 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-int main()
+main()
 {
 int s1,d1,s2,d2,s3,d3,x=1,a=2,b=3,c=4;
 scanf("%d %d %d %d %d %d",&s1,&d1,&s2,&d2,&s3,&d3);
 if(s1<=s2&&s2<=d1||s1<=d2&&d2<=d1||s1<=s3&&s3<=d1||s1<=d3&&d3<=d1){
 	x=x+1;
-	
+	a=5;
 }else a=1;
 if(s2<=s1&&s1<=d2||s2<=d1&&d1<=d2||s2<=s3&&s3<=d2||s2<=d3&&d3<=d2){
 	x=x+1;
+	b=5;
 }else b=1;
 if(s3<=s2&&s2<=d3||s3<=d2&&d2<=d3||s3<=s1&&s1<=d3||s3<=d1&&d1<=d3){
 	x=x+1;
+	c=5;
 }else c=1;
 if(s1==d2||s1==d3||s2==d1||s2==d3||s3==d1||s3==d2){
 	x=x-2;
@@ -22,9 +24,14 @@ if(s1==d2||s1==d3||s2==d1||s2==d3||s3==d1||s3==d2){
 if(s1==s2&&s2==s3&&d1==d2&&d2==d3){
 	x=3;
 }
+if(a==5&&b==5||a==5&&c==5||b==5&&c==5){
+	x=x-1;
+}
 if(a==b&&b==c){
 	x=1;
 }
 printf("%d",x);
 }
+
+
 
