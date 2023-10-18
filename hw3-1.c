5751f8ee31644dfaff97db2c210c0b3577f7cba8
bowjason <146065148+bowjason@users.noreply.github.com>
2023-10-18 21:13:26 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+main()
+{
+int x,y,z=1;
+scanf("%d",&x);
+for(int i=2;i<x;i++){
+	y=x%i;
+	if(y==0){
+		printf("no"); 
+		i=x;
+	}
+	if(i==x-1){
+		printf("yes");
+	}
+}
+if(x==2){
+		printf("yes");
+		
+	}
+}
 
