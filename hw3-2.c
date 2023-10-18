9f8e495c6cc948b89677cba01b35bfc6d1296834
bowjason <146065148+bowjason@users.noreply.github.com>
2023-10-18 21:45:11 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 main()
 {
-int s1,d1,s2,d2,s3,d3,x=1,y=1;
+int s1,d1,s2,d2,s3,d3,x=0,a=2,b=3,c=4;
 scanf("%d %d %d %d %d %d",&s1,&d1,&s2,&d2,&s3,&d3);
 if(s1<s2&&s2<d1||s1<d2&&d2<d1||s1<s3&&s3<d1||s1<d3&&d3<d1){
 	x=x+1;
-}
+	
+}else a=1;
 if(s2<s1&&s1<d2||s2<d1&&d1<d2||s2<s3&&s3<d2||s2<d3&&d3<d2){
 	x=x+1;
-}
+}else b=1;
 if(s3<s2&&s2<d3||s3<d2&&d2<d3||s3<s1&&s1<d3||s3<d1&&d1<d3){
 	x=x+1;
 }
 if(s1==s2&&s2==s3&&d1==d2&&d2==d3){
 	x=3;
+}else c=1;
+if(a==b&&b==c){
+	x=1;
 }
 printf("%d",x);
 }
-
 
