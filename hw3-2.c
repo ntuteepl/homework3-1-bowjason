7fbec7b864c6100cafba6e3218101ceb86c70a63
bowjason <146065148+bowjason@users.noreply.github.com>
2023-10-24 04:47:27 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-int s1,d1,s2,d2,s3,d3,x=0,a=0,b=0,c=0,d=0;
+int s1,d1,s2,d2,s3,d3,x=0,a=0,b=0,c=0,d=0,e=0;
 scanf("%d %d %d %d %d %d",&s1,&d1,&s2,&d2,&s3,&d3);
 
 if(d1<=s2&&b!=1){
@@ -29,6 +29,9 @@ if(c==1&&(s1<=s2&&s2<=d1||s3<=s2&&s2<=d3)){
 }
 if(d!=1){
 	x=2;
+	if(d1==s2&&s1==d3||d2==s3&&s2==d1||s1==d3&&d2==s3){
+		x=1;
+	}
 	if(c<1){
 	if(s1<=s2&&s2<=d1||s1<=d2&&d2<=d1||s1<=s3&&s3<=d1||s1<=d3&&d3<=d1){
 	
