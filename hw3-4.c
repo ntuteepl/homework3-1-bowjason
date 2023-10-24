5c5602dd420bc615d4df5e33c634a87890f71906
bowjason <146065148+bowjason@users.noreply.github.com>
2023-10-24 11:29:54 +0800

    Update hw3-4.c

--- hw3-4.c
+++ hw3-4.c
@@ -1,82 +1,45 @@
 #include <stdio.h>
-int main()
-{
-int x,y,z=0,a,b=0,c,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0,v=0,o=0,p=0;
 
-scanf("%d",&x);
-while(y!=0){
-	scanf("%d",&y);
-	o=x;
-	d=0;z=0;b=0;
-	e=x;
-	g=y;
-	p=y;
-	while(y>0){
-		a=y%10;
-		y=y/10;
-		b=b+1;
-		c=x%10;
-		x=x/10;
-		if(b==1&&a==c){
-		d=d+1;
-		n=1;
-				}
-		if(b==2&&a==c){
-		d=d+1;n=2;
-					}
-		if(b==3&&a==c){
-		d=d+1;n=3;
-					}
-		if(b==4&&a==c){
-		d=d+1;n=4;
-					}
-	
-	
-	}
-	
-	while(i<4){
-		int a[i],b[i];
-		f=g%10;
-		h=e%10;
-		a[i]=h;
-		b[i]=f;
-		e=e/10;	
-		g=g/10;
-		
-		
-		
-		if((b[0]==a[0]||b[0]==a[1]||b[0]==a[2]||b[0]==a[3])&&j!=1&&n<1){
-			z=z+1; 
-			j=1;
-			v=1;
-			
-		}
-	    if((b[1]==a[0]||b[1]==a[1]||b[1]==a[2]||b[1]==a[3])&&k!=2&&n<2){
-			z=z+1 ;
-			k=2;
-			
-		}
-		if((b[2]==a[0]||b[2]==a[1]||b[2]==a[2]||b[2]==a[3])&&l!=3&&n<3){
-			z=z+1;
-			l=3;
-			v=1;
-		}
-		if((b[3]==a[0]||b[3]==a[1]||b[3]==a[2]||b[3]==a[3])&&m!=4&&n<4){
-			z=z+1;
-			m=4;
-			
-	    }
-	
-	i=i+1; 
-	}
-	if(v!=1){
-		z=0;
-	}
-		
-	printf("%d",d);printf("A");printf("%d",z);printf("B");
-	x=o;y=p;
-	}
-	
+int main(){
+int a1,a2,b1,b2,m1,m2,t1,t2,t3,t4,A,B;
+scanf("%d", &a1);
+    while(a1<0000||a1>9999){
+    scanf("%d", &a1);
+}
+    for(int x=0;x<2;x++){
+        x = 0;
+        scanf("%d", &a2);
+    if(a2==0){
+        break;
+}
+        b1=a1;
+        b2=a2;
+        A=0;
+        B=0;
+    for(int i=0;i<4;i++){
+        m1=b1%10;
+        m2=b2%10;
+    if (m1==m2){
+        A++;
+}
+    else
+{
+        t1 = a1;
+        t2 = b2;
+    for (int j=0;j<4;j++){
+        t3=t1%10;
+        t4=t2%10;
+    if (t3==m2){
+        B++;
+}
+        t1/=10;
+        t2/=10;
+}
+}
+        b1/=10;
+        b2/=10;
 }
 
-
+printf("%dA%dB\n", A, B);
+}
+}
